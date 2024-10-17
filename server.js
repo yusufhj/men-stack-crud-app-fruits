c8e77b64631b0aa04624d0262fbdd272f42143b9
Yusuf AlHamad <ylive71@gmail.com>
2024-10-17 10:05:52 +0300

    edit finished

--- server.js
+++ server.js
@@ -49,13 +49,37 @@ app.post('/fruits', async (req, res) => {
     res.redirect("/fruits");
 });
 
+// show fruit
 app.get('/fruits/:id', async (req, res) => {
     const fruit = await Fruit.findById(req.params.id);
     // console.log(fruit);
     res.render('fruits/show.ejs', { fruit: fruit });
-    // res.send("hi")
 });
 
+// edit fruit
+app.get("/fruits/:id/edit", async (req, res) => {
+    const fruit = await Fruit.findById(req.params.id);
+    // console.log(fruit);
+    // res.send(`this is the edit route for ${fruit.name}`)
+    res.render("fruits/edit.ejs", {
+        fruit: fruit
+    });
+});
+
+app.put("/fruits/:id", async (req, res) => {
+    const formData = req.body
+    if (req.body.isReadyToEat === "on") {
+        formData.isReadyToEat = true;
+    } else {
+        formData.isReadyToEat = false;
+    }
+    await Fruit.findByIdAndUpdate(req.params.id, formData);
+
+    res.redirect(`/fruits/${req.params.id}`);
+
+})
+
+// delete fruit
 app.delete('/fruits/:id', async (req, res) => {
     await Fruit.findByIdAndDelete(req.params.id);
     res.redirect('/fruits');
